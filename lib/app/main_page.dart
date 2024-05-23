13e6fabfb05b74c26bce9ec2b31ea575976eb096
jinubee12 <jinubee12@naver.com>
2024-05-23 12:30:43 +0900

    Update_main_bottomsheet_gesture

--- lib/app/main_page.dart
+++ lib/app/main_page.dart
@@ -18,8 +18,8 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   bool _isButtonPressed = false;
 
-  void _togleButton(){
-    setState((){
+  void _togleButton() {
+    setState(() {
       _isButtonPressed = !_isButtonPressed;
     });
   }
@@ -27,20 +27,17 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: Stack(
         children: [
           Container(
-              decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
                   Colors.white,
                   Color(0xffFFD7D7),
-                ]
-              )
-            ),
+                ])),
           ),
           Center(
             child: IconButton(
@@ -49,35 +46,36 @@ class _MainPageState extends State<MainPage> {
               onPressed: _togleButton,
             ),
           ),
-          if(_isButtonPressed)
-            const Center(
-              
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                
-                children: [
-                  SizedBox(height: 300),
-                  SizedBox(height:20),
-                  CircularProgressIndicator(),
-                  SizedBox(height: 10),
-                  Text('AI에게 추천받는 중', style: TextStyle(fontSize: 16),),
-                ]
-              ),
-            ),
-          if(!_isButtonPressed)
+          if (_isButtonPressed)
             const Center(
               child: Column(
-                children: [
-                  SizedBox(height: 300),
-                  SizedBox(height:20),
-                  SizedBox(height: 10),
-                  Text('터치하여 데이트하기', style: TextStyle(fontSize: 16),),
-                ]
-              ),
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 300),
+                    SizedBox(height: 20),
+                    CircularProgressIndicator(),
+                    SizedBox(height: 10),
+                    Text(
+                      'AI에게 추천받는 중',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ]),
             ),
-            const MapBottomSheet()
+          if (!_isButtonPressed)
+            const Center(
+              child: Column(children: [
+                SizedBox(height: 300),
+                SizedBox(height: 20),
+                SizedBox(height: 10),
+                Text(
+                  '터치하여 데이트하기',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ]),
+            ),
+          if (!_isButtonPressed) const MapBottomSheet(),
         ],
-      )
+      ),
     );
   }
 }
@@ -91,14 +89,10 @@ class MapBottomSheet extends StatefulWidget {
 
 class _MapBottomSheetState extends State<MapBottomSheet> {
   late double _height;
+  late double _startHeight;
 
-  final double _lowLimit = 50;
-  final double _highLimit = 600;
-  final double _upThresh = 100;
-  final double _boundary = 500;
-  final double _downThresh = 550;
-
-  bool _longAnimation = false;
+  final double _lowLimit = 100;
+  final double _highLimit = 800;
 
   DateTime _selectedDate = DateTime.now();
   TimeOfDay _startTime = const TimeOfDay(hour: 9, minute: 0);
@@ -109,6 +103,27 @@ class _MapBottomSheetState extends State<MapBottomSheet> {
   void initState() {
     super.initState();
     _height = _lowLimit;
+  }
+
+  void _onDragUpdate(DragUpdateDetails details) {
+    setState(() {
+      _height = MediaQuery.of(context).size.height - details.globalPosition.dy;
+      if (_height < _lowLimit) {
+        _height = _lowLimit;
+      } else if (_height > _highLimit) {
+        _height = _highLimit;
+      }
+    });
+  }
+  
+  void _onDragEnd(DragEndDetails details) {
+    setState(() {
+      if (_height < (MediaQuery.of(context).size.height / 2)) {
+        _height = _lowLimit;
+      } else {
+        _height = _highLimit;
+      }
+    });
   }
 
   void _selectDate(BuildContext context) async {
@@ -146,35 +161,11 @@ class _MapBottomSheetState extends State<MapBottomSheet> {
     return Positioned(
       bottom: 0.0,
       child: GestureDetector(
-        onVerticalDragUpdate: (details) {
-          double? delta = details.primaryDelta;
-          if (delta != null) {
-            if (_longAnimation ||
-                (_height <= _lowLimit && delta > 0) ||
-                (_height >= _highLimit && delta < 0)) return;
-            setState(() {
-              if (_upThresh <= _height && _height <= _boundary) {
-                _height = _highLimit;
-                _longAnimation = true;
-              } else if (_boundary <= _height && _height <= _downThresh) {
-                _height = _lowLimit;
-                _longAnimation = true;
-              } else {
-                _height -= delta;
-              }
-            });
-          }
-        },
+        onVerticalDragUpdate: _onDragUpdate,
+        onVerticalDragEnd: _onDragEnd,
         child: AnimatedContainer(
-          curve: Curves.bounceOut,
-          onEnd: () {
-            if (_longAnimation) {
-              setState(() {
-                _longAnimation = false;
-              });
-            }
-          },
-          duration: const Duration(milliseconds: 400),
+          duration: const Duration(milliseconds: 300), // 애니메이션 시간 설정
+          curve: Curves.easeInOut,
           decoration: const BoxDecoration(
             boxShadow: [BoxShadow(blurRadius: 6, spreadRadius: 0.7)],
             color: Colors.white,
@@ -193,67 +184,97 @@ class _MapBottomSheetState extends State<MapBottomSheet> {
                   borderRadius: BorderRadius.all(Radius.circular(10)),
                 ),
               ),
-              const SizedBox(height: 20),
-              
-              // Location Selection
-              Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: _locationController,
-                      decoration: const InputDecoration(
-                        hintText: 'Location',
-                        labelText: '현재 위치',
+              const SizedBox(height: 10),
+              const Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 16.0),
+                          child: Text(
+                            '설정',
+                            style: TextStyle(
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.search),
-                    onPressed: () {
-                      // Location search logic
-                    },
-                  ),
-                ],
-              ),
-              const SizedBox(height: 16.0),
-              
-              // Date Selection
-              const Text('데이트할 날짜를 선택해 주세요!'),
-              Row(
-                children: [
-                  Text(DateFormat('yyyy-MM-dd').format(_selectedDate)),
-                  IconButton(
-                    icon: const Icon(Icons.calendar_today),
-                    onPressed: () => _selectDate(context),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 16.0),
-              
-              // Time Selection
-              const Text('데이트 시간을 정해주세요!'),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
                     children: [
-                      Text('시작 시간: ${_startTime.format(context)}'),
-                      ElevatedButton(
-                        onPressed: () => _selectTime(context, true),
-                        child: const Text('Select Start Time'),
+                      // Location Selection
+                      Row(
+                        children: [
+                          Expanded(
+                            child: TextField(
+                              controller: _locationController,
+                              decoration: const InputDecoration(
+                                hintText: 'Location',
+                                labelText: '현재 위치',
+                              ),
+                            ),
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.search),
+                            onPressed: () {
+                              // Location search logic
+                            },
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 16.0),
+
+                      // Date Selection
+                      const Text(
+                        '데이트할 날짜를 선택해 주세요!',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                      ),
+                      Row(
+                        children: [
+                          Text(DateFormat('yyyy-MM-dd').format(_selectedDate)),
+                          IconButton(
+                            icon: const Icon(Icons.calendar_today),
+                            onPressed: () => _selectDate(context),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 16.0),
+
+                      // Time Selection
+                      const Text(
+                        '데이트 시간을 정해주세요!',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            children: [
+                              Text('시작 시간: ${_startTime.format(context)}'),
+                              ElevatedButton(
+                                onPressed: () => _selectTime(context, true),
+                                child: const Text('Select Start Time'),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Text('끝나는 시간: ${_endTime.format(context)}'),
+                              ElevatedButton(
+                                onPressed: () => _selectTime(context, false),
+                                child: const Text('Select End Time'),
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                  Column(
-                    children: [
-                      Text('끝나는 시간: ${_endTime.format(context)}'),
-                      ElevatedButton(
-                        onPressed: () => _selectTime(context, false),
-                        child: const Text('Select End Time'),
-                      ),
-                    ],
-                  ),
-                ],
+                ),
               ),
             ],
           ),
@@ -262,3 +283,5 @@ class _MapBottomSheetState extends State<MapBottomSheet> {
     );
   }
 }
+
+
